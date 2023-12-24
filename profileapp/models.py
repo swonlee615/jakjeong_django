96fb444e52ff08089ed8bc214352a13fe651b4c2
swonlee615 <susanny@naver.com>
2023-12-25 03:02:35 +0900

    django course 31 commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -7,6 +7,6 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
-    image = models.ImageField(upload_to='profile/', null=True)
-    nickname = models.CharField(max_lengh=28, unique=True, null=True)
+    image = models.ImageField(upload_to='profileapp/', null=True)
+    nickname = models.CharField(max_length=28, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
