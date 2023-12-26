6b34d54336c8a5a44abdcd04c40b3080d468b5d1
swonlee615 <susanny@naver.com>
2023-12-26 19:13:29 +0900

    django course 33 commit

--- newbie/settings.py
+++ newbie/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 
 MEDIA_URL = '/media/'
 
