a6c5025acc7be936bbae46951d69fe55ae0798c3
swonlee615 <susanny@naver.com>
2023-12-30 03:55:48 +0900

    django course 37 commit

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', ArticleUpdateView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
 
 ]
